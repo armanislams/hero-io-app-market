9f1eed5e007ef26e7872915fd3d206eee73d5960
ArmanIslamTC <armanislam.ais@gmail.com>
2025-10-10 16:33:48 +0800

    fixed some error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from "react-router"; 
 import { router } from './Routes/Routes.jsx';
-import logo from '../public/logo.png'; 
 
 
 
